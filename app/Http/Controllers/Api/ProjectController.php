6649f2ee8ecd27c40df283aa43b0439dcc4d2195
robertobenfini <roberto.benfini@gmail.com>
2023-08-29 18:01:27 +0200

    [add] pagination-bonus

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -11,7 +11,7 @@ use App\Models\Project;
 class ProjectController extends Controller
 {
     public function index(){
-        $projects = Project::with('type', 'technologies')->get();
+        $projects = Project::with('type', 'technologies')->paginate(3);
         return response()->json([
             'success' => true,
             'results' => $projects
