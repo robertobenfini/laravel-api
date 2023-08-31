14345eb78407c03df29328fc35e0d061e3852282
robertobenfini <roberto.benfini@gmail.com>
2023-08-31 13:20:20 +0200

    [add] metodo-show

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -17,4 +17,21 @@ class ProjectController extends Controller
             'results' => $projects
         ]);
     }
+    
+    public function show($slug){
+        $project = Project::with('type', 'technologies')->where('slug, $slug')->first();
+
+        if($project){
+            return response()->json([
+                'success' => true,
+                'project' => $project
+            ]);
+        }
+        else{
+            return response()->json([
+                'success' => false,
+                'error' => 'Nessun progetto trovato'
+            ]);
+        }
+    }
 }
