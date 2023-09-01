2f9729ab64f38e757df87abf44436c496f4aaba9
robertobenfini <roberto.benfini@gmail.com>
2023-09-01 10:14:24 +0200

    correzione-errori

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -19,7 +19,7 @@ class ProjectController extends Controller
     }
     
     public function show($slug){
-        $project = Project::with('type', 'technologies')->where('slug, $slug')->first();
+        $project = Project::with('type', 'technologies')->where('slug', $slug)->first();
 
         if($project){
             return response()->json([
